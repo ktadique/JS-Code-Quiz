ec3baff84d7d7489da621f8d822a40c96401d6ae
ktadique <117470787+ktadique@users.noreply.github.com>
2023-01-13 15:27:15 +0000

    fixed some linking issues

---
+++ assets/js/questions.js
@@ -0,0 +1,4 @@
+//grab necessary html elements and store them into global variables
+//  seperate into other files later
+let startQuiz = document.querySelector("#start");
+let timer = document.querySelector("#time");
